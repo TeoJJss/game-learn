122806998e56d5e052bbad57911c6a4e73dc32cb
yaoyao <74146076+shengyaosiew@users.noreply.github.com>
2024-02-24 17:03:37 +0800

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -7,7 +7,7 @@
   <div class="footer">
     <div>
       <a href="#about">About Us</a>
-      <a href="#contact">Contact Us</a>
+      <a href="../public/contactus.php">Contact Us</a>
       <a href="#terms">Terms</a>
     </div>
     <hr>
