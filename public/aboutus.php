57c2eaa52532561e90d07c4a9fdd5114d2cc23a4
teojunjia <teojunjia@gmail.com>
2024-02-24 23:47:46 +0800

    gmap

--- public/aboutus.php
+++ public/aboutus.php
@@ -67,7 +67,7 @@
         <h1 class="obj_tittle">Location</h1>
 
         <div class="map">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3984.1466274575805!2d101.69798647472992!3d3.0554056969203804!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cc4abb795025d9%3A0x1c37182a714ba968!2z5Lqa5aSq56eR5oqA5aSn5a2m!5e0!3m2!1szh-CN!2smy!4v1708782500387!5m2!1szh-CN!2smy" width="100%" height="400" style="border:0;border-radius:3vh;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3984.1466274575805!2d101.69798647472992!3d3.0554056969203804!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cc4abb795025d9%3A0x1c37182a714ba968!2z5Lqa5aSq56eR5oqA5aSn5a2m!5e0!3m2!1szh-CN!2smy!4v1708782500387!5m2!1sus-EN!2smy" width="100%" height="400" style="border:0;border-radius:3vh;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
         </div>
 
